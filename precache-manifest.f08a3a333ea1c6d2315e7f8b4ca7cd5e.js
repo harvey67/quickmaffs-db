4bc772ed0c4f48407714eab5dc6706fd6130b621
harvey67 <67565077+harvey67@users.noreply.github.com>
2020-07-17 11:18:22 +1000

    Updates

---
+++ precache-manifest.f08a3a333ea1c6d2315e7f8b4ca7cd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5ec0f7d43d265dd978f75099334d9f",
+    "url": "/quickmaffs-db/index.html"
+  },
+  {
+    "revision": "52eb84641f87dd6a35e4",
+    "url": "/quickmaffs-db/static/css/main.4cf25cd4.chunk.css"
+  },
+  {
+    "revision": "1eeae6f4a7a33abed8c0",
+    "url": "/quickmaffs-db/static/js/2.65dc67b5.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/quickmaffs-db/static/js/2.65dc67b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52eb84641f87dd6a35e4",
+    "url": "/quickmaffs-db/static/js/main.5e7bf20c.chunk.js"
+  },
+  {
+    "revision": "ebea0d4735856a1031a3",
+    "url": "/quickmaffs-db/static/js/runtime-main.d1c57f3e.js"
+  }
+]);
